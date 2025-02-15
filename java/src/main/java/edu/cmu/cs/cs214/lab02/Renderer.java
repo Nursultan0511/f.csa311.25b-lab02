0f17fd6d8821db805b8d3825a0c876ce3698f28f
Nursultan <nursultandaurenbek6@gmail.com>
2025-02-15 15:10:28 +0800

    Update code: Shine code nemegdlee

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -1,19 +1,17 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.*;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Renderer {
-    public Rectangle rectangle;
-    
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    private Shape shape;  // Rectangle биш, бүх Shape төрлийн объект авах
+
+    public Renderer(Shape shape) {
+        this.shape = shape;
     }
 
-    void draw() {
-        double area = rectangle.getArea();
-
-        // assume implementation
-
+    public void draw() {
+        double area = shape.getArea();
         System.out.println("Shape printed\n" + "Its area is " + area);
     }
 }
+
